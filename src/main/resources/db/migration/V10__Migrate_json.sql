a230bd2f1bc51fb043eefb02e2502f3d2eedb2ef
Andreas Jonsson <andreas.jonsson@nav.no>
2023-08-11 11:25:39 +0200

    Migrate all rows.

--- src/main/resources/db/migration/V10__Migrate_json.sql
+++ src/main/resources/db/migration/V10__Migrate_json.sql
@@ -1,47 +1,35 @@
 UPDATE klage.text
-SET content = REPLACE(content, 'heading-one', 'h1')
-WHERE created > '2023-03-24';
+SET content = REPLACE(content, 'heading-one', 'h1');
 
 UPDATE klage.text
-SET content = REPLACE(content, 'heading-two', 'h2')
-WHERE created > '2023-03-24';
+SET content = REPLACE(content, 'heading-two', 'h2');
 
 UPDATE klage.text
-SET content = REPLACE(content, 'heading-three', 'h3')
-WHERE created > '2023-03-24';
+SET content = REPLACE(content, 'heading-three', 'h3');
 
 UPDATE klage.text
-SET content = REPLACE(content, 'heading-four', 'h4')
-WHERE created > '2023-03-24';
+SET content = REPLACE(content, 'heading-four', 'h4');
 
 UPDATE klage.text
-SET content = REPLACE(content, 'paragraph', 'p')
-WHERE created > '2023-03-24';
+SET content = REPLACE(content, 'paragraph', 'p');
 
 UPDATE klage.text
-SET content = REPLACE(content, 'bullet-list', 'ul')
-WHERE created > '2023-03-24';
+SET content = REPLACE(content, 'bullet-list', 'ul');
 
 UPDATE klage.text
-SET content = REPLACE(content, 'numbered-list', 'ol')
-WHERE created > '2023-03-24';
+SET content = REPLACE(content, 'numbered-list', 'ol');
 
 UPDATE klage.text
-SET content = REPLACE(content, 'list-item-container', 'lic')
-WHERE created > '2023-03-24';
+SET content = REPLACE(content, 'list-item-container', 'lic');
 
 UPDATE klage.text
-SET content = REPLACE(content, 'list-item', 'li')
-WHERE created > '2023-03-24';
+SET content = REPLACE(content, 'list-item', 'li');
 
 UPDATE klage.text
-SET content = REPLACE(content, 'textAlign', 'align')
-WHERE created > '2023-03-24';
+SET content = REPLACE(content, 'textAlign', 'align');
 
 UPDATE klage.text
-SET content = REPLACE(content, 'text-align-left', 'left')
-WHERE created > '2023-03-24';
+SET content = REPLACE(content, 'text-align-left', 'left');
 
 UPDATE klage.text
-SET content = REPLACE(content, 'text-align-right', 'right')
-WHERE created > '2023-03-24';
+SET content = REPLACE(content, 'text-align-right', 'right');
