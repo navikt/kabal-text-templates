826fcca94b04c0c2ad4a968770ac1f16c70def7b
Andreas Jonsson <andreas.jonsson@nav.no>
2023-10-25 16:29:48 +0200

    Debug

--- src/main/kotlin/no/nav/klage/texts/api/MaltekstseksjonController.kt
+++ src/main/kotlin/no/nav/klage/texts/api/MaltekstseksjonController.kt
@@ -113,12 +113,13 @@ class MaltekstseksjonController(
             logger = logger,
         )
 
+        val maltekstseksjonVersion = maltekstseksjonService.createNewDraft(
+            maltekstseksjonId = maltekstseksjonId,
+            versionInput = input,
+            saksbehandlerIdent = tokenUtil.getIdent(),
+        )
         return mapToMaltekstView(
-            maltekstseksjonService.createNewDraft(
-                maltekstseksjonId = maltekstseksjonId,
-                versionInput = input,
-                saksbehandlerIdent = tokenUtil.getIdent(),
-            )
+            maltekstseksjonVersion
         )
     }
 
