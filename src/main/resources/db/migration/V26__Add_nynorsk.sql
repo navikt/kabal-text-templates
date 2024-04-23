a1efa58de464c247310a1cb76e852b09fcf4ee90
Andreas Jonsson <andreas.jonsson@nav.no>
2024-04-23 14:43:17 +0200

    Format sql.

--- src/main/resources/db/migration/V26__Add_nynorsk.sql
+++ src/main/resources/db/migration/V26__Add_nynorsk.sql
@@ -1,20 +1,20 @@
 ALTER TABLE klage.text_version
-    ADD COLUMN rich_text_nn TEXT,
-    ADD COLUMN rich_text_nb TEXT,
-    ADD COLUMN plain_text_nn TEXT,
-    ADD COLUMN plain_text_nb TEXT,
+    ADD COLUMN rich_text_nn           TEXT,
+    ADD COLUMN rich_text_nb           TEXT,
+    ADD COLUMN plain_text_nn          TEXT,
+    ADD COLUMN plain_text_nb          TEXT,
     ADD COLUMN rich_text_untranslated TEXT;
 
-update klage.text_version
-    set rich_text_nb = content
-where text_type <> 'REGELVERK';
+UPDATE klage.text_version
+SET rich_text_nb = content
+WHERE text_type <> 'REGELVERK';
 
-update klage.text_version
-set rich_text_untranslated = content
-where text_type = 'REGELVERK';
+UPDATE klage.text_version
+SET rich_text_untranslated = content
+WHERE text_type = 'REGELVERK';
 
-update klage.text_version
-set plain_text_nb = plain_text;
+UPDATE klage.text_version
+SET plain_text_nb = plain_text;
 
 ALTER TABLE klage.version_editor
     ADD COLUMN change_type TEXT NOT NULL DEFAULT 'UNKNOWN';
