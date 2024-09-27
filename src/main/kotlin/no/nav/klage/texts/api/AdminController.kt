2db00a8bbf2c54d0135b33cdb70258072fceebdd
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2024-09-27 10:00:03 +0200

    End point for checking admin role.

---
+++ src/main/kotlin/no/nav/klage/texts/api/AdminController.kt
@@ -0,0 +1,35 @@
+package no.nav.klage.texts.api;
+
+import no.nav.klage.texts.config.SecurityConfiguration.Companion.ISSUER_AAD
+import no.nav.klage.texts.util.TokenUtil
+import no.nav.klage.texts.util.getLogger
+import no.nav.security.token.support.core.api.ProtectedWithClaims
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@ProtectedWithClaims(issuer = ISSUER_AAD)
+class AdminController(
+    private val tokenUtil: TokenUtil,
+) {
+
+    companion object {
+        @Suppress("JAVA_CLASS_ON_COMPANION")
+        private val logger = getLogger(javaClass.enclosingClass)
+    }
+
+    @GetMapping("/admin/rebuildcache")
+    @ResponseStatus(HttpStatus.OK)
+    fun rebuildCache() {
+
+    }
+
+    //Temporary end point for debugging
+    @GetMapping("/admin/isAdmin")
+    @ResponseStatus(HttpStatus.OK)
+    fun isAdmin(): Boolean {
+        return tokenUtil.isAdmin()
+    }
+}
--- src/main/kotlin/no/nav/klage/texts/util/TokenUtil.kt
+++ src/main/kotlin/no/nav/klage/texts/util/TokenUtil.kt
@@ -2,11 +2,13 @@ package no.nav.klage.texts.util
 
 import no.nav.klage.texts.config.SecurityConfiguration
 import no.nav.security.token.support.core.context.TokenValidationContextHolder
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
 class TokenUtil(
     private val tokenValidationContextHolder: TokenValidationContextHolder,
+    @Value("\${KABAL_ADMIN_ROLE_ID}") private val kabalAdminRoleId: String,
 ) {
 
     fun getIdent(): String =
@@ -18,4 +20,11 @@ class TokenUtil(
         tokenValidationContextHolder.getTokenValidationContext().getJwtToken(SecurityConfiguration.ISSUER_AAD)
             ?.jwtTokenClaims?.get("name")?.toString()
             ?: throw RuntimeException("Name not found in token")
+
+    fun isAdmin(): Boolean {
+        val roleIds = tokenValidationContextHolder.getTokenValidationContext().getJwtToken(SecurityConfiguration.ISSUER_AAD)
+            ?.jwtTokenClaims?.getAsList("groups")
+
+        return roleIds?.contains(kabalAdminRoleId) ?: false
+    }
 }
