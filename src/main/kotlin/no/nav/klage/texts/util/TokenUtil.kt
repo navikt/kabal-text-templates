64afd2bc8aa09ecd23b47fae6ddaa2de3881c664
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2024-09-27 10:03:58 +0200

    Test group parsing from token.

--- src/main/kotlin/no/nav/klage/texts/util/TokenUtil.kt
+++ src/main/kotlin/no/nav/klage/texts/util/TokenUtil.kt
@@ -2,15 +2,37 @@ package no.nav.klage.texts.util
 
 import no.nav.klage.texts.config.SecurityConfiguration
 import no.nav.security.token.support.core.context.TokenValidationContextHolder
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
 class TokenUtil(
     private val tokenValidationContextHolder: TokenValidationContextHolder,
+    @Value("\${KABAL_ADMIN_ROLE_ID}") private val kabalAdminRoleId: String,
 ) {
+
+    companion object {
+        @Suppress("JAVA_CLASS_ON_COMPANION")
+        private val logger = getLogger(javaClass.enclosingClass)
+        private val secureLogger = getSecureLogger()
+    }
 
     fun getIdent(): String =
         tokenValidationContextHolder.getTokenValidationContext().getJwtToken(SecurityConfiguration.ISSUER_AAD)
             ?.jwtTokenClaims?.get("NAVident")?.toString()
             ?: throw RuntimeException("Ident not found in token")
+
+    fun getName(): String =
+        tokenValidationContextHolder.getTokenValidationContext().getJwtToken(SecurityConfiguration.ISSUER_AAD)
+            ?.jwtTokenClaims?.get("name")?.toString()
+            ?: throw RuntimeException("Name not found in token")
+
+    fun isAdmin(): Boolean {
+        logger.debug("Checking if admin is available")
+        val roleIds = tokenValidationContextHolder.getTokenValidationContext().getJwtToken(SecurityConfiguration.ISSUER_AAD)
+            ?.jwtTokenClaims?.getAsList("groups")
+        logger.debug("Found roles: $roleIds")
+
+        return roleIds?.contains(kabalAdminRoleId) ?: false
+    }
 }
