646283e6a99183c361e17d11643ef44f90d717f5
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2024-10-01 13:25:34 +0200

    Adjust default values.

--- src/main/resources/db/migration/V29__Fill_out_missing_created_by.sql
+++ src/main/resources/db/migration/V29__Fill_out_missing_created_by.sql
@@ -1,8 +1,8 @@
 UPDATE klage.text
-SET created_by = 'SYSTEM'
+SET created_by = 'SYSTEMBRUKER'
 WHERE created_by IS NULL;
 
 UPDATE klage.text
-SET created_by_name = 'SYSTEM'
+SET created_by_name = 'SYSTEMBRUKER'
 WHERE created_by_name IS NULL;
 
