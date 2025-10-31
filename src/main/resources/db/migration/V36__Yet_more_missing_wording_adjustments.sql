31b08759a9f6fa8fffc3114d37726864df903694
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2025-10-31 12:27:41 +0100

    Missing rule.

--- src/main/resources/db/migration/V36__Yet_more_missing_wording_adjustments.sql
+++ src/main/resources/db/migration/V36__Yet_more_missing_wording_adjustments.sql
@@ -27,3 +27,13 @@ WHERE tvo.id in (SELECT id
                      regexp_matches(tv.rich_text_nb, '\."  "') AS matches(match_array) ON TRUE
                  WHERE matches.match_array IS NOT NULL
                    AND text_type IN ('REGELVERK', 'GOD_FORMULERING'));
+
+UPDATE klage.text_version tvo
+SET rich_text_nb =
+        REGEXP_REPLACE(rich_text_nb, ' "",', ' ",', 'g')
+WHERE tvo.id in (SELECT id
+                 FROM klage.text_version tv
+                          LEFT JOIN LATERAL
+                     regexp_matches(tv.rich_text_nb, ' "",') AS matches(match_array) ON TRUE
+                 WHERE matches.match_array IS NOT NULL
+                   AND text_type IN ('REGELVERK', 'GOD_FORMULERING'));
